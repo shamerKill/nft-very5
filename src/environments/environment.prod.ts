4e070f2b2d84cd4a00e45160e84c64c7e5be4a25
sham <sham@outlook.com>
2023-01-12 15:55:46 +0800

    [update] change serve url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   signStr: 'very5',
   // apiPath: 'http://192.168.3.117:3001/',
   // apiPath: 'http://124.248.67.122:9999/',
-  apiPath: '//server.very5.cc/',
+  apiPath: '/server/',
   marketContract: 'gx16qzcem7gzrsjmc477wstgkm8ml8pwjun8uhaa2'
 };
