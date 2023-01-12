5503fdfbbbe0646d353f176090feb2f444f89287
sham <sham@outlook.com>
2023-01-12 16:27:52 +0800

    [update] very5 to Very5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  projectName: 'very5',
+  projectName: 'Very5',
   signStr: 'very5',
   // apiPath: 'http://192.168.3.117:3001/',
   // apiPath: 'http://124.248.67.122:9999/',
