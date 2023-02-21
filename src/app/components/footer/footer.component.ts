7fcff0c38aefa397b009176fca776c71d9ad9a4d
geisheilia <gensheilia@gmail.com>
2023-02-21 10:17:08 +0800

    footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -68,6 +68,40 @@ export class FooterComponent implements OnInit {
       type: 1
     }
   ];
+  // 友情链接
+  linkTitle = $localize`Very5`;
+  linkLink: TypeLinkList = [
+    {
+      title: $localize`Telegram`,
+      link: 'https://t.me/Very5Official',
+      type: 1
+    },
+    {
+      title: $localize`Discord`,
+      link: 'https://discord.gg/mZVUrFBCph',
+      type: 1
+    },
+    {
+      title: $localize`Twitter`,
+      link: 'https://twitter.com/very5nft',
+      type: 1
+    },
+    {
+      title: $localize`YouTube`,
+      link: 'https://www.youtube.com/@Very5_Pro',
+      type: 1
+    },
+    {
+      title: $localize`Medium`,
+      link: 'https://medium.com/@very5nft',
+      type: 1
+    },
+    {
+      title: $localize`Reddit`,
+      link: 'https://www.reddit.com/user/Very5_Pro',
+      type: 1
+    }
+  ];
   goLink(link:string,type:number,key:string='') {
     if (type) {
       window.open(link)
